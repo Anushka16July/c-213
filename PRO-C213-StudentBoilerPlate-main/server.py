a3e2a11b075ba37b3e9fd061fb3dd374ffa86360
Anushka16July <87813443+Anushka16July@users.noreply.github.com>
2023-11-20 19:51:34 +0530

    Add files via upload

---
+++ PRO-C213-StudentBoilerPlate-main/server.py
@@ -0,0 +1,114 @@
+import socket
+from  threading import Thread
+from pynput.mouse import Button, Controller
+from screeninfo import get_monitors
+import autopy
+
+'''
+pynput.mouse contains classes for controlling and
+monitoring the mouse buttons & controller
+
+from screeninfo import get_monitors
+This library is used to fetch location and size of
+physical screens
+
+
+autopy: It includes functions for controlling the keyboard and
+mouse
+#pip install -U autopy
+
+#if the normal installation fails, follow these steps:
+#pip install -U setuptools -rust
+#pip install -U autopy
+
+'''
+
+
+
+SERVER = None
+PORT = 8000
+IP_ADDRESS = input("Enter your computer IP ADDR : ").strip()
+screen_width = None
+screen_height = None
+
+
+mouse = Controller()
+
+'''
+# a = "headache,fever,numbness"
+a.split(",")
+
+a = ["headache","fever", "numbness" ]
+'''
+
+
+def getDeviceSize():
+    global screen_width
+    global screen_height
+    for m in get_monitors():
+        screen_width = int(str(m).split(",")[2].strip().split('width=')[1])
+        screen_height = int(str(m).split(",")[3].strip().split('height=')[1])
+
+''''
+a= "2+90"
+print(a)---> "2+90"
+b = eval(a)
+print(b)---->92
+'''
+
+def recievemsg(client_socket):
+    global mouse
+    while True:
+        try:
+            message = client_socket.recv(2048).decode()
+            if(message):
+                new_message = eval(message)
+                if new_message["data"] == "left_click":
+                    mouse.press(Button.left)
+                    mouse.release(Button.left)
+                elif new_message["data"] == "right_click":
+                    mouse.press(Button.right)
+                    mouse.release(Button.right)
+                else:
+                    xpos = new_message["data"][0] * screen_width
+                    ypos = screen_height * (1-(new_message["data"][1]))
+                    mouse.position = (int(xpos), int(ypos))
+
+        except Exception as error:
+            pass
+
+
+def acceptConnections():
+    global SERVER
+
+    while True:
+        client_socket, addr = SERVER.accept()
+
+        print(f"Connection established with {client_socket} : {addr}")
+        
+        thread1 = Thread(target=recievemsg,args=client_socket)
+        thread1.start
+
+       
+
+
+def setup():
+    print("\n\t\t\t\t\t*** Welcome To Remote Mouse ***\n")
+
+
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+
+    SERVER.listen(10)
+
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...\n")
+
+    getDeviceSize()
+    acceptConnections()
+
+setup()
